03cc5ec63bd677c124a4d6b11288995080d2012f
Sanjar Kasmamytov <skasmamytov@gmail.com>
2020-12-24 23:29:02 +0600

    Minor: text formatting changes

--- source/functions/help.ts
+++ source/functions/help.ts
@@ -2,16 +2,16 @@ import * as Discord from 'discord.js'
 
 export async function discord_help(msg: Discord.Message) {
     const fieldHello = `
-        Say hi to a bot, and perhaps, it will say hi back! :)\n
+        Say hi to a bot, and perhaps, it will say hi back! :)
         **Example:** *+hello*`;
     const fieldClear = `
-        To clear some messages that were flooded in the text channel!\n
+        To clear some messages that were flooded in the text channel!
         **Example:** *+clear 3 (will delete 3 messages from latest)*`;
     const fieldSay = `
-        Turn your text into speech and play it in voice chat.\n
+        Turn your text into speech and play it in voice chat.
         **Example:** *+say en-us Hello, world! I am in your voice chat. (will say text starting from 'Hello, world...' in en-us language)*`;
     const fieldSayStop = `
-        Stop currently playing speech.\n
+        Stop currently playing speech.
         **Example:** *+say stop*`;
 
     const embedHelp = new Discord.MessageEmbed()
