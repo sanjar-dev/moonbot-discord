8a5bab4cc8b3eb61e2c8a6ff7dc66719b48bbc9d
Sanjar Kasmamytov <skasmamytov@gmail.com>
2020-12-24 22:59:11 +0600

    Fix "clear" command

--- source/functions/clear.ts
+++ source/functions/clear.ts
@@ -1,18 +1,21 @@
 import * as Discord from "discord.js";
 
 export async function discord_clear(msg: Discord.Message, args: string[]) {
-    if (!args.length) { return msg.channel.send('No messages count were specified - no messages were deleted!'); }
-    if (typeof(args[0]) == 'number') { return msg.channel.send('Message count argument is not a number - no messages were deleted!'); }
+    const txtchannel = msg.channel as Discord.TextChannel;
+
+    if (!args.length) { return txtchannel.send('No messages count were specified - no messages were deleted!'); }
+    if (typeof(args[0]) == 'number') { return txtchannel.send('Message count argument is not a number - no messages were deleted!'); }
 
     var msgs = parseInt(args[0]);
-    if (msgs > 100) { return msg.channel.send('Can not delete more than 100 messages - no messages were deleted!'); }
-    if (msgs < 1) { return msg.channel.send('You have to delete at least 1 message - no messages were deleted!'); }
+    if (msgs > 100) { return txtchannel.send('Can not delete more than 100 messages - no messages were deleted!'); }
+    if (msgs < 1) { return txtchannel.send('You have to delete at least 1 message - no messages were deleted!'); }
 
     const clear = async () => {
-        await msg.channel.messages.fetch({ limit: msgs }).then((msgFetched) => {
-            // msg.channel.bulkDelete(msgFetched);
-            msg.channel.send(`${args[0]} messages were deleted by ${msg.author}.`);
+        await txtchannel.messages.fetch({ limit: msgs }).then((msgFetched) => {
+            txtchannel.bulkDelete(msgFetched);
+            txtchannel.send(`${args[0]} messages were deleted by ${msg.author}.`);
         });
     }
+
     clear();
 }
--- source/functions/help.ts
+++ source/functions/help.ts
@@ -1,15 +1,27 @@
 import * as Discord from 'discord.js'
 
 export async function discord_help(msg: Discord.Message) {
+    const fieldHello = `
+        Say hi to a bot, and perhaps, it will say hi back! :)\n
+        **Example:** *+hello*`;
+    const fieldClear = `
+        To clear some messages that were flooded in the text channel!\n
+        **Example:** *+clear 3 (will delete 3 messages from latest)*`;
+    const fieldSay = `
+        Turn your text into speech and play it in voice chat.\n
+        **Example:** *+say en-us Hello, world! I am in your voice chat. (will say text starting from 'Hello, world...' in en-us language)*`;
+    const fieldSayStop = `
+        Stop currently playing speech.\n
+        **Example:** *+say stop*`;
+
     const embedHelp = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Help: commands list')
         .addFields(
-            { name: '👋 +hello', value: "Say hi to a bot, and perhaps, it will say hi back! :)\n**Example:** *+hello*" },
-            { name: '🏷️ +clear', value: "To clear some messages that were flooded in the text channel!\n**Example:** *+clear 3 (will delete 3 messages from latest)*" },
-            { name: '🎙️ +say', value: "Turn your text into speech and play it in voice chat.\n**Example:** *+say en-us Hello, world! I am in your voice chat. (will say text starting from 'Hello, world...' in en-us language)*" },
-            // { name: '🎙️ +say lang', value: "List all languages supported by text-to-speech.\n**Example:** *+say lang*" },
-            { name: '🎙️ +say stop', value: "Stop currently playing audio.\n**Example:** *+say stop*" }
+            { name: '👋 +hello', value: fieldHello },
+            { name: '🏷️ +clear', value: fieldClear },
+            { name: '🎙️ +say', value: fieldSay },
+            { name: '🎙️ +say stop', value: fieldSayStop }
         );
     msg.channel.send(embedHelp);
 }
